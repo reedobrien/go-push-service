7093e9736dbd067f754896aa6e09f849855ad81b
Reed O'Brien <reed@reedobrien.com>
2016-07-13 23:41:37 -0600

    Initial addition of skeleton to get CI setup`

---
+++ doc.go
@@ -0,0 +1,13 @@
+// wps is a web push service implementation. It is intended to implement the specs currently emerging in the following.
+//
+// Specs on IETF Tracker
+//
+// 1. [Generic Event Delivery Using HTTP Push](https://datatracker.ietf.org/doc/draft-ietf-webpush-protocol/)
+// 2. [HTTP Alternative Services](https://datatracker.ietf.org/doc/draft-ietf-httpbis-alt-svc/)
+// 3. [Message Encryption for Web Push](https://datatracker.ietf.org/doc/draft-ietf-webpush-encryption/)
+// 4. [Encrypted Content-Encoding for HTTP](https://datatracker.ietf.org/doc/draft-ietf-httpbis-encryption-encoding/)
+//
+//  W3C Push API (Client side)
+//
+// 1. [Push API](https://w3c.github.io/push-api/)
+package wps
---
+++ useragent.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2016, Reed O'Brien
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+package wps
+
+// UserAgen is mispelled to test linting. This is done so gitlab CI can be
+// configured.
+type UserAgent struct {
+	ID string
+}
+
+// NewUserAgent is a function we can test. See above.
+func NewUserAgent(id string) UserAgent {
+	return UserAgent{ID: id}
+}
