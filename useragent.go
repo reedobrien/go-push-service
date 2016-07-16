69547042966fbcbc1104775a38d3e2274e319d00
Reed O'Brien <reed@reedobrien.com>
2016-07-15 19:31:24 -0700

    Fix imports test linter

--- useragent.go
+++ useragent.go
@@ -1,15 +1,16 @@
+package wps
+
 // Copyright (c) 2016, Reed O'Brien
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-package wps
 
-// UserAgen is mispelled to test linting. This is done so gitlab CI can be
+// UserAgent is mispelled to test linting. This is done so gitlab CI can be
 // configured.
-type UserAgent struct { 
-ID string
+type UserAgent struct {
+	ID string
 }
 
 // NewUserAgent is a function we can test. See above.
 func NewUserAgent(id string) UserAgent {
-    return UserAgent{ID:    id}
+	return UserAgent{id}
 }
